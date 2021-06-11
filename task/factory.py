759d57e62394ed74e42de379426a46b8c88cacc5
Eduardo Uliana <tecnologia1@sysmo.com.br>
2021-06-11 18:59:03 -0300

    Adicionado path completo

--- task/factory.py
+++ task/factory.py
@@ -3,7 +3,8 @@ import importlib.util
 class TaskFactory():
 
     def execute(self, kind, configuration):
-        spec = importlib.util.spec_from_file_location(f"kinds.{kind}", f"kinds/{kind}.py")
+        spec = importlib.util.spec_from_file_location(f"task.kinds.{kind}", f"task/kinds/{kind}.py")
         spec_module = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(spec_module)
-        spec_module.Task(configuration).execute()
+        return spec_module.Task(configuration).execute()
+        
