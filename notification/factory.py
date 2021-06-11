a3c9c4669a4449973433fa22800fdb7c80072ddd
Rafael Gustavo Simon Werner <rafaelgustavowerner@gmail.com>
2021-06-11 18:26:15 -0300

    Adiciona factory das notificações

---
+++ notification/factory.py
@@ -0,0 +1,9 @@
+import importlib.util
+
+class NotificationFactory():
+
+    def execute(self, kind, data, message):
+        spec = importlib.util.spec_from_file_location(f"kinds.{kind}", f"kinds/{kind}.py")
+        spec_module = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(spec_module)
+        spec_module.Notification(data).execute(message)
