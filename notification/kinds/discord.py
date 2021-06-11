513b2444f861b35520a1e049d2e140ec0c5ef098
Rafael Gustavo Simon Werner <rafaelgustavowerner@gmail.com>
2021-06-11 18:25:40 -0300

    Adiciona classses dos notificadores

---
+++ notification/kinds/discord.py
@@ -0,0 +1,14 @@
+from discord_webhook import DiscordWebhook, DiscordEmbed
+
+class Discord():
+    def __init__(self, data):
+        self.__title = data['title']
+        self.__webhook = DiscordWebhook(url=data['url'], username=data['user-name'], avatar_url=data['avatar-url'])
+
+    def send(self, embed):
+        self.__webhook.add_embed(embed)
+        self.__webhook.execute()
+
+    def execute(self, message):
+        embed = DiscordEmbed(title=self.__title, description=message, color='FF0000')
+        self.send(embed)
