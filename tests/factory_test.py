89b2a7e8b43b0d7bfd47e6f998d91e0e0b584e1b
Rafael Gustavo Simon Werner <rafaelgustavowerner@gmail.com>
2021-06-13 18:04:55 -0300

    Adiciona teste para a classe factory

---
+++ tests/factory_test.py
@@ -0,0 +1,19 @@
+import unittest
+from core.factory import BaseFactory
+
+class MyFactory(BaseFactory):
+    __MODULE_NAME_FORMAT = "tests.resources.factory_test.{}"
+    __FILE_PATH_FORMAT = "tests/resources/factory_test/{}.py"
+
+    def execute(self):
+        return "ok"
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_execute_factory(self):
+        s = MyFactory('text_output').execute()
+        self.assertEqual(s, 'ok')
+
+if __name__ == '__main__':
+    unittest.main()
+
