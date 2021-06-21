333ec3d42b8bf9bcbee05b30bd9777baad479074
Rafael Gustavo Simon Werner <tecnologia3@sysmo.com.br>
2021-06-21 01:16:54 -0300

    Refatora classe de agendamento

--- core/schedule.py
+++ core/schedule.py
@@ -1,12 +1,48 @@
-from datetime import datetime, time
+from datetime import datetime, time, timedelta
 
 class Schedule():
 
-    def __init__(self, start, end, ):
-        self.__start = time.fromisoformat(start)
-        self.__end = time.fromisoformat(end)
+    def __init__(self, schedule):
+        self.__schedule = schedule
+        self.__set_interval
 
-    def is_in_period(self, start, end):
+    def __set_interval(self):
+        self.__interval = timedelta(minutes = self.__times)
+
+        if self.__schedule['kind'] == 'times':
+            self.__interval = timedelta((60 * 24) / self.__times)
+
+    def __set_next(self, value):
+        self.__schedule['next'] = value
+
+    def __kind(self):
+        return self.__schedule['kind']
+
+    def __time(self):
+        return self.__schedule['time']
+
+    def __next(self):
+        return self.__schedule['next']
+
+    def __start(self):
+        time = self.__schedule['period']['start']
+        return time.fromisoformat(time)
+
+    def __stop(self):
+        time = self.__schedule['period']['stop']
+        return time.fromisoformat(time)
+
+    def __is_in_period(self, value):
+        return (value > self.__start) and (value < self.__end)
+
+    def is_time_to_run(self):
         now = datetime.now().time()
 
-        return (now > start_time) and (now < end_time)
+        if not self.__is_in_period(now):
+            return False
+
+        if now >= self.__next:
+            self.__set_next(now + self.__interval)
+            return True
+
+        return False
