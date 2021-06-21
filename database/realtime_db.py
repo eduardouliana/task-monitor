77b41a117c472d95e8a32695b6257bc171b1ea42
Rafael Gustavo Simon Werner <tecnologia3@sysmo.com.br>
2021-06-21 01:15:47 -0300

    Adiciona listener a classe realtimedb

--- database/realtime_db.py
+++ database/realtime_db.py
@@ -5,10 +5,13 @@ from firebase_admin import db
 class RealTimeDB():
 
     def __init__(self, realtime_db_name, account_key_path = ''):
-        account_key_filepath = f'{account_key_path}/{realtime_db_name}.json'
+        account_key_filepath = f'{realtime_db_name}.json'
         cred = credentials.Certificate(account_key_filepath)
         url = f'https://{realtime_db_name}.firebaseio.com/'
         firebase_admin.initialize_app(cred, {'databaseURL': url})
+
+    def add_listener(self, node, callback):
+        db.reference(node).listen(callback)
 
     def get(self, node):
         ref = db.reference(node)
