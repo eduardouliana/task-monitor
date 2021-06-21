1c5a4e90d77516ff1620f7ac2cfac880afca5c8b
Rafael Gustavo Simon Werner <tecnologia3@sysmo.com.br>
2021-06-21 01:16:30 -0300

    Atualiza modules

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import time
 import click
 from core.runner import Runner
-from core.tasks import Tasks
+from core.configuration import Configuration
 
 def __execute(tasks, interval):
     runner = Runner(tasks)
@@ -18,18 +18,18 @@ def cli():
     """
 
 @cli.command("json-file")
-@click.option("--interval", default=30, help="Intervalo entra as execuções")
+@click.option("--interval", default=30, help="Minutos de intervalo entra as execuções")
 @click.option("--file-path", default="tasks.json", help="Arquivo contendo as tasks")
 def json_file(interval, file_path):
-    tasks = Tasks.get_from_json_file(file_path)
+    tasks = Configuration.get_from_json_file(file_path)
     __execute(tasks, interval)
 
 @cli.command()
-@click.option("--interval", default=30, help="Intervalo entra as execuções")
+@click.option("--interval", default=30, help="Minutos de intervalo entra as execuções")
 @click.option("--db-name", default="task-monitor-d4715-default-rtdb", help="Nome do RealtimeDB")
 @click.option("--data-path", default="/tasks", help="Caminho até as tasks no RealtimeDB")
 def firebase(interval, db_name, data_path):
-    tasks = Tasks.get_from_firebase(db_name, data_path)
+    tasks = Configuration.get_from_firebase(db_name, data_path)
     __execute(tasks, interval)
 
 if __name__ == '__main__':
