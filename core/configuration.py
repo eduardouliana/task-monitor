51851a9924e752a8a97d78bc0c95afcc1427a044
Rafael Gustavo Simon Werner <tecnologia3@sysmo.com.br>
2021-06-21 01:16:07 -0300

    Renomeia tasks para configuration

---
+++ core/configuration.py
@@ -0,0 +1,13 @@
+from database.realtime_db import RealTimeDB
+import json
+
+class Configuration():
+
+    def get_from_firebase(self, db_name, data_path):
+        firebase = RealTimeDB(db_name)
+        return firebase.get(data_path)
+
+
+    def get_from_json_file(self, file_path):
+        with open(file_path) as json_file:
+            return json.load(json_file)
